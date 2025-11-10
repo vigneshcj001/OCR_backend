45783b29dd8f9e94af3391c89199ebb18bce94e6
Vigneshwaran.C.J. <84329258+vigneshcj001@users.noreply.github.com>
2025-11-10 18:25:24 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,18 +1,12 @@
 #!/usr/bin/env bash
-apt-get update && apt-get install -y libleptonica-dev libtesseract-dev pkg-config \
-    libpng-dev libjpeg-dev libtiff-dev zlib1g-dev git automake autoconf libtool \
-    make g++ wget
+set -e
 
-# Build from source (v5.5.1)
-cd /tmp
-wget https://github.com/tesseract-ocr/tesseract/archive/refs/tags/5.5.1.tar.gz
-tar -xvzf 5.5.1.tar.gz
-cd tesseract-5.5.1
-./autogen.sh
-./configure
-make
-make install
-ldconfig
+# Install system dependencies
+apt-get update
+apt-get install -y tesseract-ocr libleptonica-dev libtesseract-dev
+
+# Verify installation
+tesseract --version
 
 # Install Python dependencies
 pip install -r requirements.txt
